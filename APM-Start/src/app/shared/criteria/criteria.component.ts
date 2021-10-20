22db3b1089da0d9ca6e470a01c6f3e09b6a0a702
Servando <swyswy214@gmail.com>
2021-10-20 09:33:09 -0500

    communicating with child component
    monitor parents changes in the child component and react accordingly using OnChanges lifecycle

--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -1,20 +1,28 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
 
   listFilter: string;
   @Input() displayDetail: boolean;
   @ViewChild("filterElement") filterElementRef: ElementRef;
   @Input() hitCount: number;
+  hitMessage: string;
 
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges):void {
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = "No matches found";
+    } else {
+      this.hitMessage = "Hits:" + this.hitCount;
+    }
+  }
 
   ngAfterViewInit(): void {
     if (this.filterElementRef) {
