b31077779af83d3089dd9248132a5f612b75e12c
Servando <swyswy214@gmail.com>
2021-10-24 10:50:28 -0500

    communicating through service notification
    Does the service need a subject?
    No:
    - notifications are not required
    - the only notifications are for changes to bound properties
    
    Yes:
    - notifications are required
    - these notifications are more than just changes to bound properties
    
    Which subject to use?
    Subject:
    - requires no initial value
    - broadcasts items as they are pushed
    
    BehaviorSubject:
    - requires an initial value
    - provides the current value and then broadcasts items as they are pushed

--- APM-Start/src/app/products/product-shell/product-shell-list.component.ts
+++ APM-Start/src/app/products/product-shell/product-shell-list.component.ts
@@ -1,34 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
-import { IProduct } from '../product';
-import { ProductService } from '../product.service';
+import { IProduct } from "../product";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'pm-product-shell-list',
-  templateUrl: './product-shell-list.component.html'
+  selector: "pm-product-shell-list",
+  templateUrl: "./product-shell-list.component.html",
 })
-export class ProductShellListComponent implements OnInit {
-  pageTitle: string = 'Products';
+export class ProductShellListComponent implements OnInit, OnDestroy {
+  pageTitle: string = "Products";
   errorMessage: string;
   products: IProduct[];
   selectedProduct: IProduct | null;
+  sub: Subscription[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.selectedProductChanges$.subscribe(
-      selectedProduct => this.selectedProduct = selectedProduct
+    this.sub.push(
+      this.productService.selectedProductChanges$.subscribe(
+        (selectedProduct) => (this.selectedProduct = selectedProduct)
+      )
     );
-    this.productService.getProducts().subscribe(
-      (products: IProduct[]) => {
-        this.products = products;
-      },
-      (error: any) => this.errorMessage = <any>error
+    this.sub.push(
+      this.productService.getProducts().subscribe(
+        (products: IProduct[]) => {
+          this.products = products;
+        },
+        (error: any) => (this.errorMessage = <any>error)
+      )
     );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.forEach(sub => sub.unsubscribe());
   }
 
   onSelected(product: IProduct) {
     this.productService.changeSelectedProduct(product);
   }
-
 }
